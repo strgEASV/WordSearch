0938a11a44440aa5d37091f5683bc51d25e9a15c
mirekondro1 <mirekondro@post.cz>
2025-10-29 11:04:03 +0100

    add Files prep

---
+++ src/main/java/dk/easv/dall/WordsData.java
@@ -0,0 +1,20 @@
+package dk.easv.dall;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordsData
+{
+    private List<String> Words;
+    private String WordsPath = "src/main/java/dk/easv/dal/words.txt";
+    
+    public WordsData()
+    {
+        Words = new ArrayList<>();
+    }
+
+    public List<String> getWords()
+    {
+
+    }
+}
