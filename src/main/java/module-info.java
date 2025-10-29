8bc57e767fc29664aa4e06210863bee863cc2044
mirekondro1 <mirekondro@post.cz>
2025-10-29 10:53:08 +0100

    hotfix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,7 @@
-module dk.easv.bll {
+module dk.easv.wordsearch {
     requires javafx.controls;
     requires javafx.fxml;
 
-
-    opens dk.easv to javafx.fxml;
-    exports dk.easv;
-    exports dk.easv.gui;
     opens dk.easv.gui to javafx.fxml;
+    exports dk.easv.gui;
 }
